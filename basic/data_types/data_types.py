fc7debab4bed7be4691312e7d24b3272f5ad19cf
Raj Aryan <aryan111208@gmail.com>
2024-03-02 00:20:40 +0530

    adding more basic files

---
+++ basic/data_types/data_types.py
@@ -0,0 +1,24 @@
+int_var = 10  
+str_var = "Hi Python"  
+float_var = 10.5  
+
+list_var  = [1, "hi", "Python", 2.5]   
+tuple_var = ("hi", "Python", 2)    
+set_var = {'James', 2, 3,'Python'}
+dictionary_var = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'}
+
+true_var = True
+false_var = False
+
+
+print("The type of int_var ", type(int_var))    
+print("The type of str_var ", type(str_var))  
+print("The type of float_var ", type(float_var))  
+
+print("The type of list_var ", type(list_var))  
+print("The type of tuple_var ", type(tuple_var)) 
+print("The type of set_var ", type(set_var))  
+print("The type of dictionary_var ", type(dictionary_var))  
+
+print("The type of true_var ", type(true_var))  
+print("The type of false_var ", type(false_var))  
